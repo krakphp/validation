69f1ed91146de43b7098e152aa6d8750bb06b7ef
RJ Garcia <rj@bighead.net>
2016-05-21 13:08:14 -0700

    Updating violations
    - Added new violate func to make it easier to create violations
    - Added new find_violation method to recursively find violations
    
    Signed-off-by: RJ Garcia <rj@bighead.net>

--- src/Violation.php
+++ src/Violation.php
@@ -12,9 +12,36 @@ class Violation
     public $code;
     public $params;
 
-    public function __construct($code, $params)
-    {
+    public function __construct($code, $params) {
         $this->code = $code;
         $this->params = $params;
     }
+}
+
+/** finds a violation in a violation context */
+function find_violation($violation, $code, $path = '', $sep = '.') {
+    return _find_v_rec($violation, $code, $path ? explode($sep, $path) : []);
+}
+
+function _find_v_rec($violation, $code, $path_parts) {
+    if ($violation instanceof Violation && \count($path_parts) == 0) {
+        return $violation->code == $code ? $violation : null;
+    }
+
+    if (!is_array($violation)) {
+        return;
+    }
+
+    $key = array_shift($path_parts);
+    if (!$key) {
+        return;
+    }
+
+    if (array_key_exists($key, $violation)) {
+        return _find_v_rec($violation[$key], $code, $path_parts);
+    }
+}
+
+function violate($code, $params = [null]) {
+    return new Violation($code, $params);
 }
--- src/inc.php
+++ src/inc.php
@@ -6,3 +6,4 @@ require_once __DIR__ . '/regex_validators.php';
 require_once __DIR__ . '/symfony_validators.php';
 require_once __DIR__ . '/type_validators.php';
 require_once __DIR__ . '/util_validators.php';
+require_once __DIR__ . '/Violation.php';
